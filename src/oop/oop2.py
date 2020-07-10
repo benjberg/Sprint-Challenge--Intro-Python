befcddf5620644e5e32d26be5938b1a1889277a8
Ben <bbjberg91@gmail.com>
2020-07-10 12:43:43 -0400

    mvp + stretch/changed up to pass test

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -7,7 +7,7 @@ class GroundVehicle():
     def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
     def drive(self):
-        return 'vroom'
+        return 'vroooom'
     # TODO
 
 
@@ -21,7 +21,7 @@ class Motorcycle(GroundVehicle):
     def __init__(self):
         super().__init__(num_wheels=2)
     def drive(self):
-        return 'braap!'
+        return 'BRAAAP!!'
 # TODO
 
 vehicles = [
